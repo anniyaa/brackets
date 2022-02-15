b0cf89631ac76f660ff36b377d64212d78b08ac1
Anna Vi <nibiruru@yandex.ru>
2022-02-15 18:19:25 +0300

    feat: finish brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const PAIRS = {};
+  const OPEN_BR = [];
+
+
+  for (el of [...bracketsConfig]) {
+    PAIRS[el[1]] = el[0]
+    OPEN_BR.push(el[0])
+  }
+
+  let brackets = [];
+
+  for (i=0; i<str.length; i++) {
+    let curSymbol = str[i];
+
+    if (PAIRS[curSymbol] === curSymbol ) {
+      if (brackets.includes(curSymbol)) {
+        brackets.pop()
+      } else {
+        brackets.push(curSymbol)
+      }
+    } else if (OPEN_BR.includes(curSymbol)) {
+      brackets.push(curSymbol);
+    } else if (PAIRS[curSymbol] === brackets[brackets.length-1]) {
+      brackets.pop()
+    } else {
+      return false;
+    }
+
+  }
+
+  if (brackets.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
